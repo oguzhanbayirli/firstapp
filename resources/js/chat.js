162847ca41fc52d00fa3822c23e7c02c383267d6
oguzhanbayirli <oguzhanbayirli7@gmail.com>
2026-02-04 23:19:19 +0300

    language

--- resources/js/chat.js
+++ resources/js/chat.js
@@ -201,7 +201,7 @@ export default class Chat {
             <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
             <div id="chat" class="chat-log"></div>
             <form id="chatForm" class="chat-form border-top">
-                <input type="text" class="chat-field" id="chatField" placeholder="Mesaj yaz…" autocomplete="off">
+                <input type="text" class="chat-field" id="chatField" placeholder="Type a message..." autocomplete="off">
             </form>
         `;
     }
